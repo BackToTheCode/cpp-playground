6cb1e4e6d2f5a189833b5b49a6a5b2c98c1fcf8e
James Tuckett <james@wearepoint.com>
2018-09-25 12:37:03 +0100

    More study of pointers

---
+++ pointers/pointerExample.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+int main()
+{
+    int value = 5;
+    int *ptrNormal = { nullptr };
+    int *ptr = &value; // initialize ptr with address of variable value
+ 
+    std::cout << ptrNormal << '\n'; // 
+    std::cout << &value << '\n'; // print the address of variable value
+    std::cout << ptr << '\n'; // print the address that ptr is holding
+ 
+    return 0;
+}
