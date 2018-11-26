604744f6e2dd54cac8a54ea24cb8a4683d72daca
James Tuckett <james@wearepoint.com>
2018-11-26 21:57:00 +0000

    Constant class

--- classes/oustsideClassMethodDeclarations.cpp
+++ classes/oustsideClassMethodDeclarations.cpp
@@ -28,3 +28,40 @@ void Date::SetDate(int year, int month, int day)
     m_day = day;
     m_year = year;
 }
+
+class Calc
+{
+private:
+    int m_value = 0;
+ 
+public:
+    Calc(int value=0);
+ 
+    Calc& add(int value);
+    Calc& sub(int value);
+    Calc& mult(int value);
+ 
+    int getValue() { return m_value; }
+};
+ 
+Calc::Calc(int value): m_value(value)
+{
+}
+ 
+Calc& Calc::add(int value)
+{
+    m_value += value;
+    return *this;
+}
+ 
+Calc& Calc::sub(int value) 
+{
+    m_value -= value;
+    return *this;
+}
+ 
+Calc& Calc::mult(int value)
+{
+    m_value *= value;
+    return *this;
+}
